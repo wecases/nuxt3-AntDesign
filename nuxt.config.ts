f8e292d28dde49ec9d239c7f9dcac5c5d40ef341
waset <waset@foxmail.com>
2022-03-09 15:53:26 +0800

    feat: ✨ add ant-design

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,29 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt3';
+
+import IconsResolver from 'unplugin-icons/resolver';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
-})
+    vite: {
+        plugins: [
+            Components({
+                dts: true,
+                resolvers: [
+                    IconsResolver({
+                        prefix: 'i'
+                    }),
+                    AntDesignVueResolver({
+                        resolveIcons: true
+                    })
+                ],
+            }),
+        ]
+    },
+    build: {
+        transpile: [
+            'moment', 'compute-scroll-into-view', 'ant-design-vue',
+        ],
+    },
+});
