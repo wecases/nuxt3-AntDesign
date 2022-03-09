d8f2bdf72a94fa343696ee87204a7ca526cbcec5
waset <waset@foxmail.com>
2022-03-09 20:44:20 +0800

    test: ✅  g2plot/line

---
+++ components/charts/Line.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="charts">
+        <div ref="line"></div>
+    </div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { Line } from '@antv/g2plot'
+const line = ref(null)
+
+onMounted(() => {
+    const data = [
+        { year: '1951 年', sales: 38 },
+        { year: '1952 年', sales: 52 },
+        { year: '1956 年', sales: 61 },
+        { year: '1957 年', sales: 145 },
+        { year: '1958 年', sales: 48 },
+    ]
+
+    const barPlot = new Line(line.value, {
+        data,
+        xField: 'sales',
+        yField: 'year',
+        stepType: 'vh',
+    })
+
+    barPlot.render()
+})
+</script>
---
+++ pages/index/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="" style="width: 50%; margin: 0 auto">
+        <charts-line></charts-line>
+    </div>
+</template>
