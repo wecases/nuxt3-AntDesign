7c27c77c8a12a99dbb63c8380727a489b65f0b6e
waset <waset@foxmail.com>
2022-03-10 11:36:36 +0800

    refactor: 🎨 test vueuse

--- components/charts/Line.vue
+++ components/charts/Line.vue
@@ -1,29 +1,27 @@
 <template>
     <div class="charts">
-        <div ref="line"></div>
+        <div ref="lineDom"></div>
     </div>
 </template>
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { Line } from '@antv/g2plot'
-const line = ref(null)
+import { Line, LineOptions } from '@antv/g2plot'
+
+const lineDom = ref<HTMLElement>()
+
+const props = defineProps({
+    data: {
+        required: true,
+        type: Array as () => LineOptions['data'],
+    },
+})
 
 onMounted(() => {
-    const data = [
-        { year: '1951 年', sales: 38 },
-        { year: '1952 年', sales: 52 },
-        { year: '1956 年', sales: 61 },
-        { year: '1957 年', sales: 145 },
-        { year: '1958 年', sales: 48 },
-    ]
-
-    const barPlot = new Line(line.value, {
-        data,
+    const barPlot = new Line(lineDom.value, <LineOptions>{
+        data: props.data,
         xField: 'sales',
         yField: 'year',
         stepType: 'vh',
     })
-
     barPlot.render()
 })
 </script>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,23 @@
 <template>
     <div class="w-1/2 my-0 mx-auto">
-        <charts-line></charts-line>
+        <charts-line :data="data" />
+        <div class="text-red-600">
+            <span>x:</span>
+            <span>{{ x }}</span>
+        </div>
+        <div class="text-red-600">
+            <span>y:</span>
+            <span>{{ y }}</span>
+        </div>
     </div>
 </template>
+<script setup lang="ts">
+const { x, y } = useMouse()
+const data = [
+    { year: '1951 年', sales: 38 },
+    { year: '1952 年', sales: 52 },
+    { year: '1956 年', sales: 61 },
+    { year: '1957 年', sales: 145 },
+    { year: '1958 年', sales: 48 },
+]
+</script>
